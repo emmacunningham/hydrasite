3da29f2734c2f52600e720ab3e8f04f398a94796
Emma Cunningham <the.cunning.ham@gmail.com>
2012-05-08 15:56:54 -0700

    assets loaded

--- hydra/models.py
+++ hydra/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.forms import ModelForm
+
 
 class MainPage(models.Model):
 	header = models.CharField(max_length=250)
@@ -6,3 +8,7 @@ class MainPage(models.Model):
 class NewsletterSignup(models.Model):
 	firstname = models.CharField(max_length=250)
 	email = models.EmailField(max_length=75)
+	
+class NewsletterForm(ModelForm):
+	class Meta:
+		model = NewsletterSignup
--- hydra/views.py
+++ hydra/views.py
@@ -1,10 +1,36 @@
 from django.http import HttpResponse
-from django.template import Context, loader
-from hydra.models import MainPage, NewsletterSignup
+from django.template import RequestContext, Context, loader
+from hydra.models import MainPage, NewsletterSignup, NewsletterForm
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+
+
 
 def index(request):
-    t = loader.get_template('hydra/index.html')
-    c = Context({
- 				
-    })
-    return HttpResponse(t.render(c))
+	c = {}
+	c.update(csrf(request))
+	thanks = 1
+	if request.method == 'POST': # If the form has been submitted...
+		form = NewsletterForm(request.POST, initial={'firstname': 'YOUR FIRST NAME', 'email': 'YOUR EMAIL'}) # A form bound to the POST data
+		if form.is_valid(): 
+			firstname = form.cleaned_data['firstname']
+			email = form.cleaned_data['email']
+			signup = NewsletterSignup()
+			signup.email = email
+			signup.firstname = firstname
+			signup.save()
+			return render_to_response('hydra/index.html', {
+				'form': form, 'thanks': thanks
+				},context_instance=RequestContext(request))
+	else:
+		form = NewsletterForm() # An unbound form
+
+		return render_to_response('hydra/index.html', {
+			'form': form,
+			},context_instance=RequestContext(request))
+
+			
+			
+			
+			
+
