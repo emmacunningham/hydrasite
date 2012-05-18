18568d18f4e1f57baa898603524e54f04b1d1d3c
Emma Cunningham <the.cunning.ham@gmail.com>
2012-05-17 18:54:26 -0700

    mobile responsive layout / forms still broken

--- hydrasite/settings.py
+++ hydrasite/settings.py
@@ -52,18 +52,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PARENT_DIR, 'hydramedia')
+MEDIA_ROOT = os.path.join(PARENT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/hydramedia/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PARENT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -81,6 +81,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'compressor.finders.CompressorFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -128,7 +129,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'south',
-	'hydra'
+	'hydra',
+	'compressor'
 )
 
 # A sample logging configuration. The only tangible logging
--- hydrasite/urls.py
+++ hydrasite/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^hydra/', 'hydra.views.index'),
-	url(r'^hydramedia/(?P<path>.*)$', 'django.views.static.serve',  
-	         {'document_root': settings.MEDIA_ROOT}),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve',  
+	         {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
 )
